d7f40895f7fe86f4858277fb4fa2d0555635a25f
Aidan Yagoobi <ayagoobi@Aidans-MacBook-Pro.local>
2024-09-23 21:37:11 -0400

    Updated necessary files

--- cs412/urls.py
+++ cs412/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from quotes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quotes/', include('quotes.urls')),  # Include quotes app URLs
+    path('', include('quotes.urls')),  # Include quotes app URLs
 ]
--- quotes/urls.py
+++ quotes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'), # / route
-    path('quote/', views.quote, name='quote'), #/quote route
-    path('show_all/', views.show_all, name='show_all'), 
-    path('about/', views.about, name='about'),
+    path('', views.base, name='base'),  # Home page for quotes
+    path('quote/', views.quote, name='quote'),  # Display one random quote
+    path('show_all/', views.show_all, name='show_all'),  # Display all quotes
+    path('about/', views.about, name='about'),  # About page
 ]
