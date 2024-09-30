3f562012dc7d4d593f68cf8afc0becf4c0a35fb3
Aidan Yagoobi <ayagoobi@crc-dot1x-nat-10-239-96-61.bu.edu>
2024-09-30 18:07:46 -0400

    Allowed static serving

--- cs412/settings.py
+++ cs412/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this to serve static files
 ]
 
 ROOT_URLCONF = "cs412.urls"
