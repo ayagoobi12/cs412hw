344e25547856f928c0929c90b1134a006c0b8fd5
Aidan Yagoobi <ayagoobi@crc-dot1x-nat-10-239-96-61.bu.edu>
2024-09-30 17:52:11 -0400

    Update

--- cs412/settings.py
+++ cs412/settings.py
@@ -113,8 +113,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where collectstatic will place the files
+
+# Directory for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Path for collectstatic to gather static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
