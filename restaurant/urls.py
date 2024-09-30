85c1f5e26dc4b9711ca8d84774446f83e9d4b727
Aidan Yagoobi <ayagoobi@crc-dot1x-nat-10-239-96-61.bu.edu>
2024-09-30 18:25:17 -0400

    update

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.base, name='base'),  # Main page for ordering 
+    path('main/', views.base, name='base'),  # Main page for ordering 
     path('order/', views.order, name='order'),  #Display an online order form 
     #path('confirmation/', views.confirmation, name='show_all'),  #Display a confirmation page after an order is placed 
 ]
