1045e7a6354db915f8858e1b9080341fa56fec4a
Aidan Yagoobi <ayagoobi@crc-dot1x-nat-10-239-168-21.bu.edu>
2024-10-09 13:32:27 -0400

    Finished the minifb assignment

--- minifb/views.py
+++ minifb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Profile
+from django.views.generic import ListView 
+#ListView is a generic class based view in Django that handles listing objects from a database 
 # Create your views here.
+
+class ShowAllView(ListView):
+    '''Create a subclass of ListView to display all blog articles.'''
+    model = Profile # retrieve objects of type Article from the database
+    template_name = 'minifb/show_all.html'
+    context_object_name = 'profiles' # how to find the data in the template file
+
+
