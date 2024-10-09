deaea64dc5b60826fa9bdb25b97b80a8ceced1c3
Aidan Yagoobi <ayagoobi@Aidans-MacBook-Pro.local>
2024-10-09 18:09:36 -0400

    Updating the name of show_all to show_all_profiles

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView
 class ShowAllView(ListView):
     '''Create a subclass of ListView to display all blog articles.'''
     model = Profile # retrieve objects of type Article from the database
-    template_name = 'mini_fb/show_all.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
 
 
